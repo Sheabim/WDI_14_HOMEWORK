04d1ab50f202a170aa57b1c595beb7be09cc5537
shailabrazil <sheilaadesina85@gmail.com>
2017-11-30 07:14:30 +1100

    home wed”

---
+++ Sheila/wk01/3-wed/array/ex1_ex2/ex1.js
@@ -0,0 +1,9 @@
+
+//Exercise 1 and 2 
+
+var days_of_weeks = ["monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+days_of_weeks.pop()
+
+days_of_weeks.unshift("Sunday");
+
---
+++ Sheila/wk01/3-wed/array/ex3_ex4_ex5/ex.js
@@ -0,0 +1,17 @@
+
+
+// Exercise 3 & 4 & 5
+
+var days_of_weeks = [["monday", "Tuesday", "Wednesday", "Thursday", "Friday"],["Saturday","Sunday"]];
+
+
+var removed = days_of_weeks.splice(0,1);
+
+console.log(days_of_weeks);
+
+console.log(removed);
+
+
+////??????
+
+days_of_the_weeks.sort();
