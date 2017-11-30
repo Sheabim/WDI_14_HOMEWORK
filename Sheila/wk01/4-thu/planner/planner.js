f810ea0166e7e036f1bcb930849db666fa0bd689
shailabrazil <sheilaadesina85@gmail.com>
2017-11-30 23:09:43 +1100

    ptvPlanner

---
+++ Sheila/wk01/4-thu/planner/planner.js
@@ -0,0 +1,26 @@
+
+
+//All 3 train lines intersect at Richmond, but there are NO other intersection points as trains run express.
+
+//Write a javascript program that works out display the journey when you give it an origin and destination.
+
+
+
+
+
+
+var stationS {
+
+Alamein: ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie'];
+GlenWaverly: ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga'];
+ Sandringham: ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor']
+}
+
+
+
+
+
+// HI GA INSTRUCTOR,
+
+// I COULD NOT COMPLETE THIS EXERCISE, I SPENT THE ENTIRE EVENING TRYING TO REVIEW THE PREVIOUS EXERCISES SO THAT I COULD COMPLETE THIS ONE.
+
