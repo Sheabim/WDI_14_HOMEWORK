5aeffe83721a591c48850c98de6d4a99b03d618c
shailabrazil <sheilaadesina85@gmail.com>
2018-01-06 20:59:09 +1100

    Planner update

--- Sheila/wk01/4-thu/planner/planner.js
+++ Sheila/wk01/4-thu/planner/planner.js
@@ -1,39 +1,4 @@
 
-
-//All 3 train lines intersect at Richmond, but there are NO other intersection points as trains run express.
-
-//Write a javascript program that works out display the journey when you give it an origin and destination.
-
-//DEFINE STATIOON PLUS LINES
-// TAKE USER INPUT - WE JUST ADD ANY
-// FIND TRAIN LINE OF ORIGIN + POSITION ON LINE
-
-// FIND TRAIN LINE OF FOR DESTINATION
-
-// LOOP TROUGHT ORIGIN AND DESTINATION PRINT OUT THE STATION'S NAME
-
-
-// var stations = ["Flinders Street", "Richmond", "East Richmond", "Glen Waverly"];
-// var origin = "Flinders Street";
-// var destination = "East Richmond";
-
-
-//  console.log("origin: " + origin);
-// console.log("destination: " + destination);
-
-//  var originIndex = stations.indexOf(origin);
-
-//  var destIndex = stations.indexOf(destination);
-
-// if (originIndex > -1 && destIndex > -1 ) {
-//  	console.log(destIndex - originIndex + " stops");
-//  }
-
-// var journey = stations.slice(originIndex,destIndex+1)
-//  console.log(journey.join(" ---> "))
-
-
-
 // train lines
 var lines = {
   alamein: ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
@@ -41,121 +6,120 @@ var lines = {
   sandringham: ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]
 }
 
-// user input
-var origin = prompt("origin station?")
-var destination = prompt("Destination station?")
-var originIndex
 
-// // calculating indexes
-
-var originIndex = function(origin){
-	if(lines.alamein.includes(origin) === true){
-		originIndex = lines.alamein
-
-     } else if (lines.glenWaverly.includes(origin) === true){
-          originIndex = lines.lenWaverly
-
-     	} else  {
-     	
-     	    originIndex = lines.sandringham
-     	}
+var origin = "Flinders Street";
+var destination = "Flagstaff"
+var alameinStopIndex = lines.alamein.indexOf("Richmond")
+var glenWaverlyStopIndex = lines.glenWaverly.indexOf("Richmond")
+var sandringham = lines.sandringham.indexOf("Richmond")
 
 
 
-       console.log(originIndex)
+// calculating indexes
+
+
+var originIndex = lines.alamein.indexOf(origin); //0
+
+var destIndex = lines.glenWaverly.indexOf(destination);//2
+
+//check if there are in the same line
+
+
+// route the origin to stopover
+
+var originToStopover = lines.alamein.slice(originIndex, alameinStopIndex + 1)
+//console.log(originToStopover)
+
+// route the stopover to destination
+
+var stopOverToDestination;
+
+
+if (destIndex < glenWaverlyStopIndex){
+	stopOverToDestination= lines.glenWaverly.slice(destIndex, glenWaverlyStopIndex +1 );  
 
 }
-// if(lines.alamein.indexOf(origin)){
+else{
+   stopOverToDestination= lines.glenWaverly.slice(glenWaverlyStopIndex, destIndex +1 );  
+
+}
+stopOverToDestination = stopOverToDestination.reverse()
+stopOverToDestination.splice(0,1)
+
+	console.log(stopOverToDestination);
+
+//concat arrays
+
+var fullRoute = originToStopover.concat(stopOverToDestination)
+	console.log(fullRoute)
 
 
-// 	} else if (var originIndex = lines.glenWaverly.indexOf(origin)){
-	
+	console.log("origin " + origin)
+	console.log("destination" + destination)
+	console.log(fullRoute.join(" ----->"))
 
-// 	} else if (var originIndex = lines.sandringham.indexOf(origin)) {
+
+// // // calculating number of stops
+// for (var line in lines){// for every element in the object 
+// 	if( lines.hasOwnProperty(line) ) { 
+//       console.log(lines[line]);
+//     } 
 // }
- 
+
+//look for origin line
 
 
 
-// var originIndex = lines.station.indexOf(origin);
-// var destIndex = lines.lines.indexOf(destination);
+
+
+
+// // creating journey
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // user input
+// var origin = "Flinders Street";
+// var destination = "East Richmond";
+
+// // calculating indexes
+// var originIndex = lines.alamein.indexOf(origin); //0
+// var destIndex = lines.alamein.indexOf(destination);//2
 
 // // calculating number of stops
-// if (originIndex > -1 && destIndex > -1 ) {
-// 	var numberOfStops = destIndex - originIndex;
+// if (originIndex > -1 && destIndex > -1 ) { //
+//   var numberOfStops = destIndex - originIndex;
 // }
 
 // // creating journey
 // var journey = lines.alamein.slice(originIndex, destIndex+1)
 
 // var outputToUser = function() {
-// 	console.log("origin: " + origin);
-// console.log("destination: " + destination);
+//   console.log("origin: " + origin);
+//   console.log("destination: " + destination);
 
-// 	console.log(numberOfStops + " stops");
-// // 	console.log(journey.join(" ---> "))
-//  }
+//   console.log(numberOfStops + " stops");
+//   console.log(journey.join(" ---> "))
+// }
 
 // outputToUser()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //DEFINE STATIOON PLUS LINES
-// var stations = ['Flinders', 'Richmond', 'East Richmond']
-
-
-
-// // TAKE USER INPUT - WE JUST ADD ANY
-
-// var origin = "Flinders";
-// var destination = "East richmond ";
-// var journey = "";
-
-// console.log('origin:'+ origin);
-// console.log('destination: ' + destination);
-
-
-// // FIND TRAIN LINE OF ORIGIN + POSITION ON LINe
-
-// // FIND TRAIN LINE OF FOR DESTINATION // 
-// //DIFERNCE B/W ORIGIN/DEST INDEX
-
-// // var originIndex = stations.indexOf(origin);
-// // var destIndex = stations.indexOf(destination);
-
-// // if(originIndex > -1 && destIndex > -1){
-// // console.log(destIndex - originIndex + " stops");
-
-// // }
-
-
-
-// // LOOP TROUGHT ORIGIN AND DESTINATION PRINT OUT THE STATION'S NAME
-// for(var i = originIndex; i <= destIndex; i++){ // we need to have a counter 0,1,2,3,4
-// 	if(i===destIndex){
-//     journey += stations[i] 
-
-// 	} 
-// 	else {journey += stations[i]+"--->"
-// }
-// }
-
-// console.log(journey);
-
-
-// // ARRAY HOLD ITEMS ARE SIMILAR
-
-
