0055a22ded49b22936fda83873ddef6bb1a791da
shailabrazil <sheilaadesina85@gmail.com>
2018-01-10 07:38:01 +1100

    Movie part2

--- Sheila/wk05/1-mon/movies_app/main.rb
+++ Sheila/wk05/1-mon/movies_app/main.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
 
-
+#@title is an instance variable and title is a local variable 
 
 get '/' do
   erb :index
@@ -17,12 +17,27 @@ erb :about
 end
 
 
-
 get '/movie' do 
 
 @url = "http://omdbapi.com/?apikey=2f6435d9&t=" + params[:movie_title] # always like that
 @movie_search = HTTParty.get(@url) 
-@movie_search.parsed_response
+@movie_search.parsed_response #METHOD OF HTTPPARTY
 erb :movie
 
 end
+
+
+
+get '/options' do
+
+@url = "http://omdbapi.com/?apikey=2f6435d9&s=" + params[:movie_title]
+@movie_search = HTTParty.get(@url) 
+
+erb :options
+
+end
+
+
+
+
+
